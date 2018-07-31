17fa3fd10d0bc645b19e01d761eeba866f802f83
luchang <123456>
2018-07-31 11:23:36 +0800

    初始化blogger1

--- src/main/java/com/blog/app/domain/Blogger.java
+++ src/main/java/com/blog/app/domain/Blogger.java
@@ -1,9 +1,18 @@
 package com.blog.app.domain;
 
+import org.hibernate.annotations.GenericGenerator;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "blogger")
 public class Blogger {
+
+    @Id
+    @GeneratedValue(generator="generator")
+    @GenericGenerator(name="generator",strategy="uuid")
+    private String id;
 }
